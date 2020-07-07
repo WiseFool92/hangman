d4d7c1802e1d2bbc29aae82163f8dd2afbf248de
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-07 15:48:53 -0700

    Btns for inputs

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Hangman.css';
 import { randomWord } from './Words.jsx';
+import { Button } from 'reactstrap';
 
 import step0 from './img/0.png';
 import step1 from './img/1.png';
@@ -25,12 +26,27 @@ class Hangman extends Component {
     }
   }
 
+  // NEED TO REFACTOR SO BLANK SPACES DO NOT GET UNDERSCORES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   guessedWord() {
     return this.state.answer.split("").map(letter => (this.state.guessed.has(letter) ? letter : " _ "));
   }
 
+  generateButtons() {
+    return "abcdefghijklomnopqrstuvwxyz".split("").map(letter => (
+      <Button
+        key={letter} 
+        value={letter}
+        onClick={this.handleGuess}
+        diasabled={this.state.guessed.has(letter)}
+      >
+        {letter}
+      </Button>
+    ))
+  }
+
   render() {
     const gameOver = this.state.mistake >= this.props.maxWrong;
+    let gameStat = this.generateButtons();
     
     return (
       <div className="Hangman container">
@@ -44,7 +60,10 @@ class Hangman extends Component {
           <h4><strong>Guess the Movie Title: </strong></h4>
           <p>
             {!gameOver ? this.guessedWord() : this.state.answer}
-          </p> 
+          </p>
+          <div>
+            <p>{gameStat}</p>
+          </div>
         </div>
       </div>
     )
