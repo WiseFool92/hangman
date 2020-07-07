091e962f74cb003da97038235b8751a648346e0d
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-07 15:23:07 -0700

    img displays

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './Hangman.css';
 import { randomWord } from './Words.jsx';
 
-import step0 from './Img/0.png';
-import step1 from './Img/1.png';
-import step2 from './Img/2.png';
-import step3 from './Img/3.png';
-import step4 from './Img/4.png';
-import step5 from './Img/5.png';
-import step6 from './Img/6.png';
+import step0 from './img/0.png';
+import step1 from './img/1.png';
+import step2 from './img/2.png';
+import step3 from './img/3.png';
+import step4 from './img/4.png';
+import step5 from './img/5.png';
+import step6 from './img/6.png';
 
 class Hangman extends Component {
   static defaultProps = {
     maxWrong: 6,
-    images: [step0, step1, step2, step3, step4, step5, step6]
+    img: [step0, step1, step2, step3, step4, step5, step6]
   }
 
   constructor(props) {
@@ -25,15 +25,17 @@ class Hangman extends Component {
     }
   }
 
-
   render() {
     return (
-      <div>
+      <div className="Hangman container">
         <h1>Hangman</h1>
-        <p>{this.state.answer}</p>
+        <div class="GuessBoxLocation">Wrong Guesses: {this.state.mistake} of {this.props.maxWrong}</div>
+        <div class="ImgLocation">
+          <img src={this.props.img[this.state.mistake]} alt=""/>
+        </div>
       </div>
     )
-  }
+  } 
 }
 
 export default Hangman;
